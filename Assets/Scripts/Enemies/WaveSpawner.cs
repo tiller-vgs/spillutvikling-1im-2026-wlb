1a19f3672c7bd4703f92b8131cfac32479aa99b7
kloroid <stormgustavsen@gmail.com>
2026-03-02 14:53:57 +0100

    I just made a way better spawning system, the only thing that needs change is making a wave start/stop system

---
+++ Assets/Scripts/Enemies/WaveSpawner.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _enemyPrefab;
+
+    [SerializeField]
+    private float _minimumSpawnTime;
+
+    [SerializeField]
+    private float _maximumSpawnTime;
+
+    private float _timeUntilSpawn;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        SetTimeUntilSpawn();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _timeUntilSpawn -= Time.deltaTime;
+
+        if (_timeUntilSpawn <= 0)
+        {
+            Instantiate(_enemyPrefab, transform.position, Quaternion.identity);
+            SetTimeUntilSpawn();
+        }
+    }
+    private void SetTimeUntilSpawn()
+    {
+        _timeUntilSpawn = Random.Range(_minimumSpawnTime, _maximumSpawnTime);
+    }
+}
